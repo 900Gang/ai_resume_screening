69c6c2c73fed7b22bf50070966311400a7939588
900Gang <anandanand6776@gmail.com>
2026-08-28 08:51:12 +0530

    adding canonical forms

--- phrase_test.py
+++ phrase_test.py
@@ -4,56 +4,54 @@ from spacy.matcher import PhraseMatcher
 nlp = spacy.load("en_core_web_sm")
 # Define SKILLS
 SKILLS = {
-    "python",
-    "sql",
-    "html",
-    "css",
-    "javascript",
-    "django",
-    "flask",
-    "postgresql",
-    "docker",
-    "git",
-    "github",
-    "firebase",
-    "linux",
-    "jenkins",
-    "machine learning",
-    "data science",
-    "natural language processing"
+    "python": "Python",
+    "sql": "SQL",
+    "html": "HTML",
+    "css": "CSS",
+    "javascript": "JavaScript",
+    "django": "Django",
+    "flask": "Flask",
+    "postgresql": "PostgreSQL",
+    "docker": "Docker",
+    "git": "Git",
+    "github": "GitHub",
+    "firebase": "Firebase",
+    "linux": "Linux",
+    "jenkins": "Jenkins",
+    "machine learning": "Machine Learning",
+    "data science": "Data Science",
+    "natural language processing": "Natural Language Processing"
 }
 
 # Create PhraseMatcher
-matcher = PhraseMatcher(nlp.vocab)
+matcher = PhraseMatcher(nlp.vocab, attr="LOWER")
+
+# creatng matchid
+# skill_keys = list(SKILLS.keys())
 
 # Create patterns
 patterns = [nlp.make_doc(skill) for skill in SKILLS]
-
 # Add patterns
 matcher.add("SKILL",patterns)
 
 def extract_skills(text):
-    detected_skills=[]
-    # Create doc from text
+    detected_skills = set()
+
     doc = nlp(text)
-    
-    # Run matcher
+
     matches = matcher(doc)
-    
-    # Loop through matches
-    
+
     for match_id, start, end in matches:
-        
-        # Convert each match into span.text
         span = doc[start:end]
-        
-        # Add it to detected_skills
-        detected_skills.append(span.text)
-        
-        # Return detected_skills
-        return detected_skills
+        skill_key = span.text.lower()
+        canonical_skill = SKILLS[skill_key]
+        detected_skills.add(canonical_skill)
+
+    return list(detected_skills)
+
+
     
-    if __name__ == "__main__":
+if __name__ == "__main__":
         sample_text = """
     I have experience with Python, Django,
     Machine Learning and GitHub.
